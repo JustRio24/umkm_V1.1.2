e8a441247e418ba3e939de4616ab51885018baf3
RIO <justrio24@gmail.com>
2025-10-26 02:37:12 +0700

    fix Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Mie Mercon Jawara')</title>
-    <link rel="icon" href="{{ asset('images/logo_mie_jawara.jpeg') }}" type="image/jpeg">
+    <link rel="icon" type="image/jpeg" href="{{ asset('logo_mie_jawara.jpeg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -211,7 +211,7 @@
 
             <a class="navbar-brand" href="{{ route('home') }}">
                 <img 
-                    src="{{ asset('images/logo_mie_jawara.jpeg') }}" 
+                    src="{{ asset('logo_mie_jawara.jpeg') }}" 
                     style="height: 40px; margin-right: 10px;"
                 >
                 Mie Mercon Jawara
